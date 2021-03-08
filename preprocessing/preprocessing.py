b0f682e84b258fbc19c8f6196e7edf858ee28fa6
gieeedreee <66144591+gieeedreee@users.noreply.github.com>
2021-03-09 00:43:14 +0200

    Add files via upload

---
+++ preprocessing/preprocessing.py
@@ -0,0 +1,28 @@
+import pickle
+import json
+import numpy as np
+import pandas as pd
+
+
+with open("model/encoder.pkl", "rb") as f:
+    encoder = pickle.load(f)
+
+with open("model/scaler.pkl", "rb") as s:
+    scaler = pickle.load(s)
+
+
+def process_input(request_data: str) -> np.array:
+    """
+    Requested data is encoded, scaled and prepared for further  use.
+    :return: input values and encoded/ scaled values
+    """
+    try:
+        input_df = pd.DataFrame(json.loads(request_data)["features"])
+        scaled_input = scaler.transform(input_df[['Area', 'Room', 'Year', 'Flat_floor', 'Total_floor']])
+        encoded_input = pd.DataFrame(encoder.transform(input_df[['City']]))
+        final_input = np.concatenate([scaled_input, encoded_input], axis=-1, )
+
+        return final_input, input_df
+    except ValueError:
+        raise
+
